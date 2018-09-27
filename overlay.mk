b91ff87b63cca3e1353452592e286499a3ad7d27
Pierre-Hugues Husson <phh@phh.me>
2018-09-27 14:03:37 +0200

    Delete hw-fpnav. This is now handled by Treble app
    Conflicts:
    	overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -51,11 +51,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     HwCamera2
-
-PRODUCT_PACKAGES += \
-	hw-fpnav-daemon
-PRODUCT_COPY_FILES += \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fingerprint.rc:system/etc/init/hw-fingerprint.rc \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav:system/bin/hw-fpnav \
-	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex \
-	vendor/hardware_overlay/Huawei/Fingerprint/fingerprint.kl:system/phh/huawei/fingerprint.kl
